2435af37a31a6f897e57808a626b97f38ba18302
Ellie Johnston (Byte the Fox) <155034127+EJxByteTheFox@users.noreply.github.com>
2025-02-22 08:55:27 -0500

    Create Arm.java

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,14 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Arm {
+    private final TalonFX liftDriver 
+        = new TalonFX(0, "rio");
+    private final Slot0Configs liftConf
+        = new Slot0Configs()
+            .withkP(0)
+            .withkI(0)
+            .withkD(0);
+            
+}
