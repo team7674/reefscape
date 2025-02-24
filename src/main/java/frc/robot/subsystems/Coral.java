2d38f3d6a148054a04dff08d82e2bf3bf63b0d8c
7674programming <raiderbots5@gmail.com>
2025-02-24 16:02:55 -0500

    push

---
+++ src/main/java/frc/robot/subsystems/Coral.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Coral extends SubsystemBase{
+
+    private final Servo flapperServo = new Servo(0); //creates the servo to allow coral in
+
+    public void close() {
+        flapperServo.set(0);
+    }
+
+    public void half() {
+        flapperServo.set(.5);
+    }
+
+    public void open() {
+        flapperServo.set(1);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/vision/util/VisionUtil.java
@@ -0,0 +1,51 @@
+package frc.robot.subsystems.vision.util;
+
+import java.util.List;
+
+
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public final class VisionUtil {
+
+    public static final double kCameraHeight = 0.85935;
+    public static final double kCameraPitch = 0;
+    public static final Transform2d cameraToRobot = new Transform2d(0.0, 0.2286, Rotation2d.fromRadians(0.0));
+
+    private VisionUtil() {}
+
+    protected static final double kAvgCrazyLow = 0.00;
+    protected static final double kAvgCrazyHigh = 28.00;
+
+    public static Pose2d averagePoses2D(List<Pose2d> poses) {
+        if ( poses.isEmpty() )
+            return new Pose2d();
+
+        double sumX = 0.00;
+        double sumY = 0.00;
+        double sumT = 0.00;
+        int badPoses = 0;
+
+        for ( Pose2d pose : poses ) {
+            if ( pose.getX() > kAvgCrazyHigh || pose.getX() < kAvgCrazyLow || pose.getY() > kAvgCrazyHigh || pose.getY() < kAvgCrazyLow ) {
+                badPoses++;
+                continue;
+            }
+
+            sumX += pose.getX();
+            sumY += pose.getY();
+            sumT += pose.getRotation().getRadians();
+        }
+
+        double avgX = (sumX / (poses.size() - badPoses));
+        double avgY = (sumY / (poses.size() - badPoses));
+        double avgT = (sumT / (poses.size() - badPoses));
+
+        System.out.println(avgX);
+        System.out.println(avgY);
+        System.out.println(avgT);
+        System.out.println(badPoses);
+        return new Pose2d(avgX, avgY, new Rotation2d(avgT));
+    }
+}
